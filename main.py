7367638c562ef95f19792bac5df4cca684be051f
Virvi Huta <hutavirvi07@gmail.com>
2026-03-03 12:44:55 +0100

    connection was succesful

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import mysql.connector
 
+connection = mysql.connector.connect(
+    host='htl-datenbank.com',
+    port=28474,
+    user='virhut23',
+    password='REMOVED',
+    database='virhut23_hotel_db'
+)
+
+if connection.is_connected():
+    print("Connection was Successful!")
