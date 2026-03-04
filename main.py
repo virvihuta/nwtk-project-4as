690e532b636bb0a2bda711726a846beb7967ff75
Virvi Huta <hutavirvi07@gmail.com>
2026-03-04 15:11:10 +0100

    fixed security

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+user = os.getenv("user")
+password = os.getenv("password")
 
 connection = mysql.connector.connect(
     host='htl-datenbank.com',
