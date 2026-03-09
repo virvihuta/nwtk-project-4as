f3c9f4d3c60124f13709ed1b341ac9c0e9bb9ae3
Virvi Huta <hutavirvi07@gmail.com>
2026-03-09 11:51:55 +0100

    created a guest booking function

--- main.py
+++ main.py
@@ -15,14 +15,38 @@ connection = mysql.connector.connect(
     database='virhut23_hotel_db'
 )
 
-if connection.is_connected():
-    print("Connection is Successful!")
+cursor = connection.cursor()
 
-    cursor = connection.cursor()
-    cursor.execute("select * from guests")
-    results = cursor.fetchall()
-    for i in results:
-        print(i)
+
+def create_guest_booking(first_name, last_name, email, phone, room_id, check_in, check_out):
+
+    sql_guest = """
+    INSERT INTO guests (first_name, last_name, email, phone)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(sql_guest, (first_name, last_name, email, phone))
+    guest_id = cursor.lastrowid
+
+
+    sql_booking = """
+    INSERT INTO bookings (guest_id, room_id, check_in, check_out)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(sql_booking, (guest_id, room_id, check_in, check_out))
+
+    connection.commit()
+
+    print("Guest and booking created successfully!")
+
+
+
+if connection.is_connected():
+
+    print("Connection Successful!")
+
+    create_guest_booking("Virvi", "Huta", "virvi@gmail.com", "123456", 1, "2026-06-01", "2026-06-05")
 
 else:
-    print("Connection is not Successful!")
+    print("Connection Failed")
